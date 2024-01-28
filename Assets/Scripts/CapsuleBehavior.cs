92d3b8c2a7b62ea50142afb451a672f6b8cf64df
Jan Heinz <heinz.j@northeastern.edu>
2024-01-28 16:07:44 -0500

    finished CapsuleBehavior

--- Assets/Scripts/CapsuleBehavior.cs
+++ Assets/Scripts/CapsuleBehavior.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class CapsuleBehavior : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class CapsuleBehavior : MonoBehaviour {
+    public Transform target;
+
+    private float warningDistance = 3.0f;
+    private string message = "";
+    
+    void Update() {
+        Vector3 directionToTarget = target.position - transform.position;
+        float distanceToTarget = Vector3.Distance(target.position, transform.position);
+        float dotProduct = Vector3.Dot(transform.forward, directionToTarget);
         
+        if (distanceToTarget < warningDistance) {
+            if (dotProduct > 0.2) { // padding of .2 for approximating right angles
+                message = gameObject.name + ": Cube is in front";
+            } else if (dotProduct < -0.2) {
+                message = gameObject.name + ": Cube is behind";
+            } else {
+                message = gameObject.name + ": Cube is at a right angle";
+            }
+        } else {
+            message = ""; // Clear message when the target is too far
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+    void OnGUI() {
+        GUI.Button(new Rect(20, 20, 250, 40), message);
+        }
     }
-}
+
+
+    
+
