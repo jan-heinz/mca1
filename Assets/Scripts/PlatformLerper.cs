a66933198be4f77572479f03654a7577e4fc1df8
Jan Heinz <heinz.j@northeastern.edu>
2024-01-28 16:14:25 -0500

    finished PlatformLerper

--- Assets/Scripts/PlatformLerper.cs
+++ Assets/Scripts/PlatformLerper.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlatformLerper : MonoBehaviour {
+    public float speed = 4.0f;
+    public Color initialColor;
+    public Color endColor;
+
+    Renderer renderer;
+    
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        renderer = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        // code from lecture notes
+        float t = Mathf.Sin(Time.time * speed);
+        t += 1;
+        t /= 2;
+
+        renderer.material.color = Color.Lerp(initialColor, endColor, t);
     }
 }
